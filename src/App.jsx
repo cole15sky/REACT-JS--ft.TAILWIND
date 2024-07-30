1ec01af0ec55fd5f95641dc343efb62da4a5fb4b
Sky Blue <dawadiaakash@gmail.com>
2024-07-30 16:38:06 +0545

    Initialize Tailwind and Vite react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-xl font-bold text-blue-500'>Tailwind Css with react vite.</h1>
+    
+    </>
+  )
+}
+
+export default App
