27f09ba3e49a3928719f97d4f4bb5afb00f11ae0
Sky Blue <dawadiaakash@gmail.com>
2024-07-30 23:50:54 +0545

    Logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,31 +3,35 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   const content = <>
-  <div className="">
-    <ul>
-      <link to="Home">
-      <li>Home</li>
-      </link>
-      <link>
-      <li to="About">About</li>
-      </link>
-      <link>
-      <li to="Blogs">Blogs</li>
-      </link>
-      <link>
-      <link to="Gallery">Gallery</link>
-      </link>
-    </ul>
-    
+    <div className="">
+      <ul>
+        <link to="Home">
+          <li>Home</li>
+        </link>
+        <link>
+          <li to="About">About</li>
+        </link>
+        <link>
+          <li to="Blogs">Blogs</li>
+        </link>
+        <link>
+          <link to="Gallery">Gallery</link>
+        </link>
+      </ul>
+
     </div></>
   return (
     <nav>
-
+      <div>
+        <div>
+          <span> logo </span>
+        </div>
+      </div>
     </nav>
-    
+
   );
-    
-  
+
+
 };
 
 export default Navbar
