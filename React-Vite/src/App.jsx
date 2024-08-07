e938cd7182b9d44baab9990e911fd38ae5fc0e25
Sky Blue <dawadiaakash@gmail.com>
2024-08-07 22:12:53 +0545

    Tailwind and props

--- React-Vite/src/App.jsx
+++ React-Vite/src/App.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Components/Card'
 
-const App = ()=>{
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Passenger",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
+
   return (
-    <div className="bg-slate-900">
-   <Nav/>
-    </div>
-  );
-};
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="sky" btnText="click me" />
+      <Card username="dido" />
+    </>
+  )
+}
 
-export default App;
+export default App
