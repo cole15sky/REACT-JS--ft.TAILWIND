d62c2f746d0bf570e68e7faa5b502776a2db2c28
Sky Blue <dawadiaakash@gmail.com>
2024-08-09 23:29:36 +0545

    Add Placeholder

--- Projects/Project 2/my-project/src/App.jsx
+++ Projects/Project 2/my-project/src/App.jsx
@@ -12,15 +12,33 @@ function App() {
     let string ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghojklmnopqrstuvwxyz"  
   if(allowNumber) str +="0123456789"
   if(allowChar) str +="!@#$%^&*()_+?:"
+
+  for(i=1; i <=array.length; i++){
+    let char = Math.floor(Math.random()*string.length +1)
+    pass = str.charAt(char)
+  }
+  setPassword(pass)
+
+
+
+  
  
   
   },[length,allowNumber,allowChar])
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div className="w-full max-w-md mx-auto shadow-md
+    rounded-lg px-4 my-8 text-orange-500 bg-gray-800 ">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+    <div className='className="flex shadow rounded-lg overflow-hidden mb-4"'>
+      <input type="text"
+      value={password}
+      className="outline-none w-full py-1 px-3 "
+      placeholder="password"
+      readOnly
+      
+      />
+      </div> 
+    </div>
   )
 }
 
