17aba98cdce7c5b2e259d8d8c64fc29e547461ea
Sky Blue <dawadiaakash@gmail.com>
2024-08-09 23:02:25 +0545

    UseState hooks

--- Projects/Project 2/my-project/src/App.jsx
+++ Projects/Project 2/my-project/src/App.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [length,setLength] = useState(8)
+  const [allowNumber,setAllowNumber] = useState(false);
+  const [allowChar,setAllowChar] = useState(false); 
+  const [password, setPassword] = useState("") 
   return (
     <>
       <h1 className="text-3xl font-bold underline">
