6648530ac4e452b8c9745b7a363e5156aa52b179
Sky Blue <dawadiaakash@gmail.com>
2024-08-09 18:42:27 +0545

    Execute callback function on onclick buttons

--- 1.Bg-changer/my-project/src/App.jsx
+++ 1.Bg-changer/my-project/src/App.jsx
@@ -12,28 +12,38 @@ function App() {
     
     <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-red
     rounded-3xl "> 
-    <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
+    <button onClick={() => setColor("red")}
+    style={{backgroundColor:"Red"}}
+     className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
+      Red </button>
+    <button onClick={() => setColor("Blue")}
+    style={{backgroundColor:"Blue"}}
+     className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
       Blue </button>
-    <button 
+    <button onClick={() => setColor("Yellow")}
     style={{backgroundColor:"Yellow"}}
     className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
       Yellow </button>
-    <button
+    <button onClick={() => setColor("Pink")}
     style={{backgroundColor:"Pink"}}
     className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
       Pink </button>
-    <button
+    <button onClick={() => setColor("Olive")}
     style={{backgroundColor:"Olive"}}
      className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
       Olive </button>
-    <button 
+    <button onClick={() => setColor("Green")}
     style={{backgroundColor:"Green"}}
     className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
       Green </button>
-    <button
+    <button onClick={() => setColor("White")}
     style={{backgroundColor:"White"}}
      className="outline-none px-4 py-1 rounded-full text-black shadow-lg"> 
       White </button>
+      <button onClick={() => setColor("Black")}
+    style={{backgroundColor:"Black"}}
+     className="outline-none px-4 py-1 rounded-full text-white shadow-lg"> 
+      Black </button>
 
     </div>    
     </div>
